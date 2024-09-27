b8215baa227885cb2c3c0eb31953dd6b7c2e8292
mk06005@gmail.com <mk065005@gmail.com>
2024-09-27 18:50:27 +0530

    init

---
+++ client/next.config.mjs
@@ -0,0 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'pm-s3-images.s3.us-east-2.amazonaws.com',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
+  
