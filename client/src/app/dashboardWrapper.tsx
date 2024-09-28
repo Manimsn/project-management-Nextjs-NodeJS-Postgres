52a6bd8449dc3c899cf5a32de490eb4e80755a3d
mk06005@gmail.com <mk065005@gmail.com>
2024-09-28 23:52:37 +0530

    Added sign in sign up form

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,28 +1,33 @@
-"use client"
+"use client";
+
 import React, { useEffect } from "react";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import AuthProvider from "./authProvider";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   useEffect(() => {
-    if(isDarkMode){
-      document.documentElement.classList.add("dark")
-    }else{
-      document.documentElement.classList.remove("dark")
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
-  })
+  });
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* Sidebar */}
       <Sidebar />
-
-      <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSidebarCollapsed ? '':'md:pl-64' } `}>
-        {/* navbar */}
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
+      >
         <Navbar />
         {children}
       </main>
@@ -31,11 +36,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-return (
-  <StoreProvider>
-    <DashboardLayout>{children}</DashboardLayout>
-  </StoreProvider>
-)
-}
+  return (
+    <StoreProvider>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
+    </StoreProvider>
+  );
+};
 
 export default DashboardWrapper;
