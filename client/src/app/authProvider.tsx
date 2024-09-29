746deb559981b173d280b2914312d4853d00879a
mk06005@gmail.com <mk065005@gmail.com>
2024-09-29 13:06:15 +0530

    adding to do list

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
+import Image from "next/image";
 
 Amplify.configure({
   Auth: {
@@ -44,18 +45,30 @@ const formFields = {
 
 const AuthProvider = ({ children }: any) => {
   return (
-    <div>
-      <Authenticator formFields={formFields}>
-        {({ user }: any) =>
-          user ? (
-            <div>{children}</div>
-          ) : (
-            <div>
-              <h1>Please sign in below:</h1>
-            </div>
-          )
-        }
-      </Authenticator>
+    <div className="flex h-screen">
+      <div className="w-3/5 flex items-center justify-center p-8">
+        <Image
+          className="object-cover rounded-md w-[85%] h-[100%]"
+          src="/images/idcolablogin.png"
+          alt=""
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="w-2/5 flex items-center justify-center">
+        <Authenticator formFields={formFields}>
+          {({ user }: any) =>
+            user ? (
+              <div>{children}</div>
+            ) : (
+              <div>
+                {/* TFO */}
+                <h1>Please sign in below:</h1>
+              </div>
+            )
+          }
+        </Authenticator>
+      </div>
     </div>
   );
 };
